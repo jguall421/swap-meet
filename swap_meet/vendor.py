d190d602362576b316cfcc2aca93b998d58bc120
Jesica Guallpa <guallpajesica65@gmail.com>
2025-09-29 11:34:56 -0700

    Wave 1 w/o fixing test

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        self.inventory = [] if inventory is None else inventory
+    
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
+      
