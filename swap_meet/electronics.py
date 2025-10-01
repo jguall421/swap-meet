63ce44830b97184921b5a4c7512a2773280c2da0
Jesica Guallpa <guallpajesica65@gmail.com>
2025-10-01 13:51:44 -0700

    Wave 5 w/Inheritance added

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,15 @@
-class Electronics:
-    pass
+from uuid import uuid4
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, id = None, type = "Unknown", condition = 0):
+        super().__init__(id = id, condition = condition)
+        self.type = type
+        
+    def get_category(self):
+        return "Electronics"
+    
+    def __str__(self):
+        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
+    
+
